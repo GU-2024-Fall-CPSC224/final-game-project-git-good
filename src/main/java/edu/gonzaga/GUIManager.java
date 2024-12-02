5e52abe53fe7a72a9cedbbacb45e9aa85dbda532
Parker Shore <parkershore@gmail.com>
2024-12-02 00:33:15 -0800

    Worked on the start menu
    Also added gui manager class

---
+++ src/main/java/edu/gonzaga/GUIManager.java
@@ -0,0 +1,25 @@
+package edu.gonzaga;
+
+public class GUIManager {
+    StartScreen startScreen = new StartScreen();
+    startScreen.runGUI();
+
+    // void runGUI() {
+    //     System.out.println("Starting Game");
+    //     setupGUI();
+
+    //     // These methods are to show you how it works
+    //     // Once you get started working, you can comment them out so they don't
+    //     //  mess up your own code.
+    //     //putDemoDefaultValuesInGUI();
+    //     addDemoButtonCallbackHandlers();
+
+    //     // Right here is where you could methods to add your own callbacks
+    //     // so that you can make the game actually work.
+
+    //     // Run the main window - begins GUI activity
+    //     mainWindowFrame.setVisible(true);
+    //     //System.out.println("Done in GUI app");
+    // }
+    
+}
--- src/main/java/edu/gonzaga/StartScreen.java
+++ src/main/java/edu/gonzaga/StartScreen.java
@@ -1,38 +1,46 @@
 package edu.gonzaga;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 class StartScreen {
 
     private JFrame mainWindowFrame;
-    private JPanel controlPanel;
-    private JPanel scorecardPanel;
+    // private JPanel controlPanel;
+    // private JPanel scorecardPanel;
 
-    // Dice view, user input, reroll status, and reroll button
-    private JTextField diceValuesTextField;
-    private JTextField diceKeepStringTextField;
-    private JButton diceRerollBtn;
-    private JTextField rerollsLeftTextField;
+    // // Dice view, user input, reroll status, and reroll button
+    // private JTextField diceValuesTextField;
+    // private JTextField diceKeepStringTextField;
+    // private JButton diceRerollBtn;
+    // private JTextField rerollsLeftTextField;
 
-    // Player name - set it to your choice
-    private JTextField playerNameTextField = new JTextField();
+    // // Player name - set it to your choice
+    // private JTextField playerNameTextField = new JTextField();
 
     // Buttons for showing dice and checkboxes for meld include/exclude
-    ArrayList<JButton> diceButtons = new ArrayList<>();
-    ArrayList<JCheckBox> meldCheckboxes = new ArrayList<>();
+    ArrayList<JButton> playerButtons = new ArrayList<>();
+    // ArrayList<JCheckBox> meldCheckboxes = new ArrayList<>();
 
-    private JButton calcMeldButton = new JButton("Calc Meld");
-    private JTextField diceDebugLabel = new JTextField();
-    private JLabel meldScoreTextLabel = new JLabel();
-    private JButton rollButton = new JButton();
+    // private JButton calcMeldButton = new JButton("Calc Meld");
+    // private JTextField diceDebugLabel = new JTextField();
+    // private JLabel meldScoreTextLabel = new JLabel();
+    // private JButton rollButton = new JButton();
+    private JButton playButton = new JButton("Play");
 
-    private JPanel playerInfoPanel = new JPanel();
-    private JPanel diceControlPanel = new JPanel();
-    private JPanel meldControlPanel = new JPanel();
+    //private JPanel playerInfoPanel = new JPanel();
+    private JPanel playPanel = new JPanel();
+    private JPanel playerPanel = new JPanel();
 
     public StartScreen() {
         setupGUI();
@@ -43,32 +51,39 @@ class StartScreen {
         this.mainWindowFrame = new JFrame("Simple GUI Farkle");
         this.mainWindowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.mainWindowFrame.setLocation(100, 100);
+        this.mainWindowFrame.setSize(800, 600);
 
         // Player info and roll button panel
-        this.playerInfoPanel = genPlayerInfoPanel();
+        //this.playerInfoPanel = genPlayerInfoPanel();
 
         // Dice status and checkboxes to show the hand and which to include in the meld
-        this.diceControlPanel = genDiceControlPanel();
+        this.playPanel = genPlayPanel();
 
         // The bottom Meld control panel
-        this.meldControlPanel = genMeldControlPanel();
+        this.playerPanel = genPlayerPanel();
 
-        mainWindowFrame.getContentPane().add(BorderLayout.NORTH, this.playerInfoPanel);
-        mainWindowFrame.getContentPane().add(BorderLayout.CENTER, this.diceControlPanel);
-        mainWindowFrame.getContentPane().add(BorderLayout.SOUTH, this.meldControlPanel);
+        //mainWindowFrame.getContentPane().add(BorderLayout.NORTH, this.playerInfoPanel);
+        mainWindowFrame.getContentPane().add(BorderLayout.CENTER, this.playPanel);
+        mainWindowFrame.getContentPane().add(BorderLayout.SOUTH, this.playerPanel);
         mainWindowFrame.pack();
     }
 
-    private JPanel genMeldControlPanel() {
+    private JPanel genPlayerPanel() {
         JPanel newPanel = new JPanel();
-        newPanel.setLayout(new FlowLayout());
+        newPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        newPanel.setLayout(new GridLayout(1, 7, 1, 1));
+        JLabel playerCountLabel = new JLabel("Player count:");
+        newPanel.add(playerCountLabel);
 
-        JLabel meldScoreLabel = new JLabel("Meld Score:");
-        this.meldScoreTextLabel.setText("0");
-        
-        newPanel.add(calcMeldButton);
-        newPanel.add(meldScoreLabel);
-        newPanel.add(this.meldScoreTextLabel);
+        for(Integer index = 1; index <= 4; index++) {
+            //JLabel colLabel = new JLabel(index.toString(), SwingConstants.CENTER);
+            JButton playerCountButton = new JButton(index.toString() + " players");
+            if (index == 1){
+                playerCountButton = new JButton(index.toString() + " player");
+            }
+            this.playerButtons.add(playerCountButton);
+            newPanel.add(playerCountButton);
+        }
 
         return newPanel;
     }
@@ -84,34 +99,11 @@ class StartScreen {
      *
      * @return A JPanel containing components for dice control.
      */
-    private JPanel genDiceControlPanel() {
+    private JPanel genPlayPanel() {
         JPanel newPanel = new JPanel();
         newPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        newPanel.setLayout(new GridLayout(3, 7, 1, 1));
-        JLabel diceLabel = new JLabel("Dice Vals:");
-        JLabel meldBoxesLabel = new JLabel("Meld 'em:");
 
-        newPanel.add(new Panel());  // Upper left corner is blank
-        for(Integer index = 0; index < 6; index++) {
-            //JLabel colLabel = new JLabel(index.toString(), SwingConstants.CENTER);
-            JLabel colLabel = new JLabel(Character.toString('A' + index), SwingConstants.CENTER);
-            newPanel.add(colLabel);
-        }
-        newPanel.add(diceLabel);
-
-        for(Integer index = 0; index < 6; index++) {
-            JButton diceStatusButton = new JButton("D" + index.toString());
-            this.diceButtons.add(diceStatusButton);
-            newPanel.add(diceStatusButton);
-        }
-
-        newPanel.add(meldBoxesLabel);
-        for(Integer index = 0; index < 6; index++) {
-            JCheckBox meldCheckbox = new JCheckBox();
-            meldCheckbox.setHorizontalAlignment(SwingConstants.CENTER);
-            this.meldCheckboxes.add(meldCheckbox);
-            newPanel.add(meldCheckbox);
-        }
+        newPanel.add(playButton);
 
         return newPanel;
     }
@@ -127,38 +119,38 @@ class StartScreen {
      *
      * @return A JPanel containing components for player information.
      */
-    private JPanel genPlayerInfoPanel() {
-        JPanel newPanel = new JPanel();
-        newPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        newPanel.setLayout(new FlowLayout());    // Left to right
+    // private JPanel genPlayerInfoPanel() {
+    //     JPanel newPanel = new JPanel();
+    //     newPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+    //     newPanel.setLayout(new FlowLayout());    // Left to right
 
-        JLabel playerNameLabel = new JLabel("Player name:");
-        playerNameTextField.setColumns(20);
-        diceDebugLabel.setColumns(6);
-        diceDebugLabel.setHorizontalAlignment(SwingConstants.CENTER); // Center the text
-        rollButton.setText("Roll Dice");
+    //     JLabel playerNameLabel = new JLabel("Player name:");
+    //     playerNameTextField.setColumns(20);
+    //     diceDebugLabel.setColumns(6);
+    //     diceDebugLabel.setHorizontalAlignment(SwingConstants.CENTER); // Center the text
+    //     rollButton.setText("Roll Dice");
 
 
-        newPanel.add(playerNameLabel);   // Add our player label
-        newPanel.add(playerNameTextField); // Add our player text field
-        newPanel.add(rollButton);        // Put the roll button on there
-        newPanel.add(this.diceDebugLabel);
+    //     newPanel.add(playerNameLabel);   // Add our player label
+    //     newPanel.add(playerNameTextField); // Add our player text field
+    //     newPanel.add(rollButton);        // Put the roll button on there
+    //     newPanel.add(this.diceDebugLabel);
 
-        return newPanel;
-    }
+    //     return newPanel;
+    // }
 
 
     /*
      *  This is a method to show you how you can set/read the visible values
      *   in the various text widgets.
      */
-    private void putDemoDefaultValuesInGUI() {
-        // Example setting of player name
-        this.playerNameTextField.setText("Player One");
+    // private void putDemoDefaultValuesInGUI() {
+    //     // Example setting of player name
+    //     this.playerNameTextField.setText("Player One");
 
-        // Example Dice debug output
-        this.diceDebugLabel.setText("124566");
-    }
+    //     // Example Dice debug output
+    //     this.diceDebugLabel.setText("124566");
+    // }
 
     /*
      * This is a demo of how to add callbacks to the buttons
@@ -166,57 +158,63 @@ class StartScreen {
      */
     private void addDemoButtonCallbackHandlers() {
         // Example of a button callback - just prints when clicked
-        this.rollButton.addActionListener(new ActionListener() {
+        // this.rollButton.addActionListener(new ActionListener() {
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         System.out.println("They clicked the roll button!");
+        //         diceButtons.get(2).setText("Rolled");
+        //         diceDebugLabel.setText("67321");
+        //     }
+        // });
+        this.playButton.addActionListener((ActionListener) new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("They clicked the roll button!");
-                diceButtons.get(2).setText("Rolled");
-                diceDebugLabel.setText("67321");
+                System.out.println("Starting game");
             }
         });
 
         // Example of another button callback
         // Reads the dice checkboxes and counts how many are checked (selected)
         // Sets the meldScoreTextLabel to how many of the checkboxes are checked
-        this.calcMeldButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Integer boxCheckCount = 0;
-                for(JCheckBox checkBox : meldCheckboxes) {
-                    if(checkBox.isSelected()) {
-                        boxCheckCount++;
-                    }
-                }
-                System.out.println("Setting meld score text");
-                meldScoreTextLabel.setText(boxCheckCount.toString());
-            }
-        });
+        // this.calcMeldButton.addActionListener(new ActionListener() {
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         Integer boxCheckCount = 0;
+        //         for(JCheckBox checkBox : meldCheckboxes) {
+        //             if(checkBox.isSelected()) {
+        //                 boxCheckCount++;
+        //             }
+        //         }
+        //         System.out.println("Setting meld score text");
+        //         meldScoreTextLabel.setText(boxCheckCount.toString());
+        //     }
+        // });
 
         // Example of a checkbox handling events when checked/unchecked
-        JCheckBox boxWithEvent = this.meldCheckboxes.get(1);
-        boxWithEvent.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(boxWithEvent.isSelected()) {
-                    System.out.println("Checkbox is checked");
-                } else {
-                    System.out.println("Checkbox is unchecked");
-                }
-            }
-        });
+        // JCheckBox boxWithEvent = this.meldCheckboxes.get(1);
+        // boxWithEvent.addActionListener(new ActionListener() {
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         if(boxWithEvent.isSelected()) {
+        //             System.out.println("Checkbox is checked");
+        //         } else {
+        //             System.out.println("Checkbox is unchecked");
+        //         }
+        //     }
+        // });
     }
 
     /*
      *  Builds the GUI frontend and allows you to hook up the callbacks/data for game
      */
-    void runGUI() {
-        System.out.println("Starting GUI app");
+    public void runGUI() {
+        System.out.println("Starting Game");
         setupGUI();
 
         // These methods are to show you how it works
         // Once you get started working, you can comment them out so they don't
         //  mess up your own code.
-        putDemoDefaultValuesInGUI();
+        //putDemoDefaultValuesInGUI();
         addDemoButtonCallbackHandlers();
 
         // Right here is where you could methods to add your own callbacks
@@ -224,6 +222,6 @@ class StartScreen {
 
         // Run the main window - begins GUI activity
         mainWindowFrame.setVisible(true);
-        System.out.println("Done in GUI app");
+        //System.out.println("Done in GUI app");
     }
 }
